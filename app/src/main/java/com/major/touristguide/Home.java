e0788f9ede47a7d969c6727d280dceec583b732a
Aashna Budhiraja <aashna.budhiraja.ab@gmail.com>
2018-10-02 16:51:06 +0530

    Add Google Maps Polyline Route

--- app/src/main/java/com/major/touristguide/Home.java
+++ app/src/main/java/com/major/touristguide/Home.java
@@ -1,7 +1,10 @@
 package com.major.touristguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        createNewTrip();
+    }
+
+    public void createNewTrip() {
+        Button createNewTripButton = (Button)findViewById(R.id.create_trip_button_home);
+        createNewTripButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Home.this, MapsActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
